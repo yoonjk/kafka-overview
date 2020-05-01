ed186caa3f749d3de74d530d0760c0800421e219
nexweb1 <nexweb1@gmail.com>
2020-05-01 21:00:23 +0900

    Install kafka/connect/schema-registry to kubernetes.

---
+++ practitioner/kafka-clean.sh
@@ -0,0 +1,22 @@
+helm del my --purge
+for i in {0..2}
+do
+  kubectl delete pvc datadir-0-my-cp-kafka-${i}
+done
+
+for i in {0..2}
+do
+  kubectl delete pvc datadir-my-cp-zookeeper-${i}
+done
+for i in {0..2}
+do
+  kubectl delete pvc datalogdir-my-cp-zookeeper-${i}
+done
+
+helm del my-schema --purge
+helm del my-connect --purge
+helm del my-ksql --purge
+# schema-registry
+kubectl delete pvc datadir-schema-registry-kafka-0
+
+
