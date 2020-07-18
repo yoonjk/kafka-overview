5a085d44e2e8ce8f2798304e2057709e8302e6b3
nexweb1 <nexweb1@gmail.com>
2020-07-18 09:58:57 +0900

    Add demo

--- practitioner/kafka-clean.sh
+++ practitioner/kafka-clean.sh
@@ -15,7 +15,7 @@ done
 
 helm del my-schema --purge
 helm del my-connect --purge
-helm del my-ksql --purge
+#helm del my-ksql --purge
 # schema-registry
 kubectl delete pvc datadir-schema-registry-kafka-0
 
